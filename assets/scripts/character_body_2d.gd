f2cd693580d07d76477e4f685969f53c50324b5e
Aaron Phung <user.name>
2025-02-20 22:20:56 -0800

    implemented functionality for spawning hitbox frames for mining

--- assets/scripts/character_body_2d.gd
+++ assets/scripts/character_body_2d.gd
@@ -70,22 +70,42 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _input(event) -> void:
+	if event.is_action_pressed("ui_accept"):
+		mine()
+
+func mine() -> void:
+	match state:
+		States.IDLE, States.MOVING:
+			set_state(States.MINING)
+			match _animation_player.current_animation:
+				"move_left", "idle_left":
+					_animation_player.play("mine_left")
+				"move_right", "idle_right":
+					_animation_player.play("mine_right")
+				"move_up", "idle_up":
+					_animation_player.play("mine_up")
+				"move_down", "idle_down":
+					_animation_player.play("mine_down")
+
 func set_state(new_state: int) -> void:
 	match new_state:
-		States.IDLE:
+		0:
 			state = new_state
 			match (_animation_player.current_animation):
-				"move_left":
+				"move_left", "mine_left":
 					_animation_player.play("idle_left")
-				"move_right":
+				"move_right", "mine_right":
 					_animation_player.play("idle_right")
-				"move_up":
+				"move_up", "mine_up":
 					_animation_player.play("idle_up")
-				"move_down":
+				"move_down", "mine_down":
 					_animation_player.play("idle_down")
 				_:
 					_animation_player.play("idle_down")
-		States.MOVING:
+		1:
 			state = new_state
-		States.MINING:
+		2:
 			state = new_state
+			velocity.y = 0
+			velocity.x = 0
---
+++ assets/scripts/hitbox_controller.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@export var frame_paths: Array[String] = [
+	"res://assets/prefabs/mining/mine_down.tscn",
+	"res://assets/prefabs/mining/mine_left.tscn",
+	"res://assets/prefabs/mining/mine_right.tscn",
+	"res://assets/prefabs/mining/mine_up.tscn"]
+
+var frames: Array[Resource] = []
+var active_frame = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	for frame_path in frame_paths:
+		frames.append(load(frame_path))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+func draw_hitbox(id: int) -> void:
+	if active_frame:
+		active_frame.queue_free()
+		active_frame = null
+	active_frame = frames[id].instantiate()
+	active_frame.set_name("hitbox")
+	add_child(active_frame)
+
+func clear_hitbox() -> void:
+	if active_frame:
+		active_frame.queue_free()
+		active_frame = null
