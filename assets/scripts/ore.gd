632b163ef6bd816c9e6a69061631cd08fdc5504b
Aaron Phung <user.name>
2025-03-04 00:01:04 -0800

    created ore.gd script

---
+++ assets/scripts/ore.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+enum OreType { COPPER, NICKEL, SILVER, TUNGSTEN, IRIDIUM}
+
+@export var ore_type: OreType = OreType.COPPER
+@export var hp: int = 5
+
+@onready var sprite = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func on_hit(dmg: int) -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
