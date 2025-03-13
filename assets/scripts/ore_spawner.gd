420f19b0c8601c2cf1b101b1865e0bccf610b1cf
Aaron Phung <user.name>
2025-03-12 18:21:43 -0700

    implemented random ore generation

--- assets/scripts/ore_spawner.gd
+++ assets/scripts/ore_spawner.gd
@@ -8,10 +8,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if (spawnChance < randf_range(0.0, 1.0)):
-		ore.set_process(false)
+	generate_ore()
 	pass # Replace with function body.
 
+func generate_ore() -> void:
+	var a = randf_range(0.0, 1.0)
+	if (spawnChance < a):
+		ore.queue_free()
+	else:
+		var oreIndex = randi_range(0, len(orePool) - 1)
+		ore.ore_type = orePool[oreIndex]
+		ore.update_sprite()
+		var amountIndex = randi_range(0, len(amountPool) - 1)
+		ore.ore_amount = amountPool[amountIndex]
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
