da19da79dc412900dbf221ad5602ff3d261143b2
Aaron Phung <user.name>
2025-04-28 19:22:48 -0700

    each game scene now has a corresponding scene id, and calls the ObjectiveManager as a quest completion trigger

--- scenes/game_scene.gd
+++ scenes/game_scene.gd
@@ -6,6 +6,20 @@ class_name GameScene extends Node2D
 signal transition_scene(scene: String, spawnPosition: Vector2)
 
 @export var transitions: Array[SceneTransition]
+@export var scene_id: SceneId
+
+enum SceneId {
+	NONE,
+	MAIN_BASE,
+	MINING_AREA,
+	LAB,
+	ZIMBO,
+	STATION,
+	HOUSE1,
+	HOUSE2,
+	HOUSE3
+}
+
 
 
 
@@ -14,6 +28,7 @@ func _ready() -> void:
 	for transition in transitions:
 		transition.transition_enter.connect(_on_transition_enter)
 	GameManager.instance.set_camera_bounds(camera_bounds)
+	ObjectiveManager.instance.on_scene_load(scene_id)
 
 func _on_transition_enter(scene: String, spawn_position: Vector2):
 	transition_scene.emit(scene, spawn_position)
