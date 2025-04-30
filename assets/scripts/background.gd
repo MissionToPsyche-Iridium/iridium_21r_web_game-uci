6c5507f80cdce58a45dc7efd4dc49cc8756fee67
Aaron Phung <user.name>
2025-04-30 16:08:57 -0700

    set initial parameters in _ready func

--- assets/scripts/background.gd
+++ assets/scripts/background.gd
@@ -9,6 +9,8 @@ var initialY: float = position.y
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	initialX = position.x
+	initialY = position.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
