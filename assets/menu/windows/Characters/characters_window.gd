89f2535abd32ee89c446a24f5953e06dc738e38b
Noban Tahir <mntahir@uci.edu>
2025-05-15 22:14:35 -0700

    Completed character scenes.

--- assets/menu/windows/Characters/characters_window.gd
+++ assets/menu/windows/Characters/characters_window.gd
@@ -11,9 +11,30 @@ func _ready():
 	lab_button.pressed.connect(_on_lab_button_pressed)
 	close_button.pressed.connect(_on_close_button_pressed)
 
+	$TextureRect/Icons/Col1/Claire.pressed.connect(_on_claire_button_pressed)
+	$TextureRect/Icons/Col1/Yuno.pressed.connect(_on_yuno_button_pressed)
+	$TextureRect/Icons/Col2/Dee.pressed.connect(_on_dee_button_pressed)
+	$TextureRect/Icons/Col2/Zimbo.pressed.connect(_on_zimbo_button_pressed)
+	$TextureRect/Icons/Col3/Skibby.pressed.connect(_on_skibby_button_pressed)
+
 func _on_lab_button_pressed():
 	WindowManager.close_window()
 	WindowManager.open_window("res://assets/menu/windows/Lab/LabWindow.tscn")
 
 func _on_close_button_pressed():
 	WindowManager.close_window()
+	
+func _on_claire_button_pressed():
+	WindowManager.open_window("res://assets/menu/windows/Characters/CharactersScenes/Claire.tscn")
+
+func _on_yuno_button_pressed():
+	WindowManager.open_window("res://assets/menu/windows/Characters/CharactersScenes/Yuno.tscn")
+
+func _on_dee_button_pressed():
+	WindowManager.open_window("res://assets/menu/windows/Characters/CharactersScenes/Dee.tscn")
+
+func _on_zimbo_button_pressed():
+	WindowManager.open_window("res://assets/menu/windows/Characters/CharactersScenes/Zimbo.tscn")
+
+func _on_skibby_button_pressed():
+	WindowManager.open_window("res://assets/menu/windows/Characters/CharactersScenes/Skibby.tscn")
