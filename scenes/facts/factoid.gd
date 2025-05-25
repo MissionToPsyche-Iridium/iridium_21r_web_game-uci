e0545b9fb6f385475c5559146267a7be765b3116
aaronphung998 <wacky700@gmail.com>
2025-05-24 22:48:02 -0700

    factoid checks if already interacted with upon loading

--- scenes/facts/factoid.gd
+++ scenes/facts/factoid.gd
@@ -8,14 +8,20 @@ var fact_id = "explosion"
 var factoid = "The Pysche asteroid is most likely a survivor of multiple violent hit-and-run collisions, which was common when the solar system was forming."
 
 func _ready() -> void:
+	
 	sprite.texture = load(ResourceManager.itemIcons[item_id])
+	if ResourceManager.instance:
+		var has_interacted: bool = ResourceManager.instance.has_amount(item_id, 1)
+		interactable.is_interactable = !has_interacted
+		self.visible = !has_interacted
 	interactable.interact = _on_interact
 	
 func _on_interact():
-	interactable.is_interactable = false
-	Global.has_explosion = true
-	self.visible = false
-	if ResourceManager.instance:
-		ResourceManager.instance.add_to_inventory(item_id, 1)
-	print("get interacted")
-	Dialogic.start_timeline("call_from_claire")
+	if interactable.is_interactable:
+		interactable.is_interactable = false
+		Global.has_explosion = true
+		self.visible = false
+		if ResourceManager.instance:
+			ResourceManager.instance.add_to_inventory(item_id, 1)
+		print("get interacted")
+		Dialogic.start_timeline("call_from_claire")
